f3a4534d042ad060e90ed089f9cd53e99099e807
Sayem-khan <sayemkhan8920@gmail.com>
2025-08-05 20:59:51 +0600

    first c programme

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+// this is my first programme
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d%% %d%%", a, b);
+    return 0;
+}
---
+++ veriable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+     int rahim = 100, karim = 300;
+     int karim = 200;
+     printf("%d %d", rahim, karim);
+     float pie = 3.1416;
+     printf("%.3f", pie);
+    char satem = 'U';
+     printf("%c", satem);
+    return 0;
+}
