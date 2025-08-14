b931563d58a4f8a7f51e36a93169dbaa95609a79
Md. Sayem Khan <sayemkhan8920@gmail.com>
2025-08-14 19:05:06 +0600

    Added 2nd type Loop
    This code about Module-3.Here all are Loop related C programme .

---
+++ loop_2.c
@@ -0,0 +1,15 @@
+// This code about For loop type-2
+
+#include <stdio.h>
+int main()
+{
+    int i;
+    for (i = 1; i <= 10; i = i++) // Same as loop_1
+
+    {
+        printf("%d\n", i); /*becuse computar ar memory te already
+         i name ar akta veriable call kora ache*/
+    }
+
+    return 0;
+}
